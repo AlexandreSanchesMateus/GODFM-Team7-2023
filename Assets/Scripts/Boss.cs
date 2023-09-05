9a1981b565e0e2f576fc88c246d478eb1ebd1b31
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2023-09-05 15:28:40 +0200

    PlayerManager Modified

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,33 +1,78 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Boss : MonoBehaviour
 {
-    public enum State
+    public enum EBossState
     {
-        one,
-        two,
-        three,
-        four
+        ATTACK_ONE,
+        ATTACK_TWO,
+        ATTACK_THREE,
+        ATTACK_FOUR,
+        CHOSE_ATTACK,
+        VULNERABLE,
+        RECOVER,
+        DEATH,
+        NONE,
     }
-    public State state;
 
-    // Start is called before the first frame update
+    private EBossState _currentState;
+    private EBossState _previousState;
+    private float _attackSpeed;
+
+    [SerializeField] private float _delayBetweenAttack;
+
     void Start()
     {
-        
+        _currentState = EBossState.NONE;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        switch (state)
+        if (_previousState == _currentState) return;
+
+        switch (_currentState)
         {
-            
+            case EBossState.CHOSE_ATTACK:
+                StartCoroutine(DelayState((EBossState)Random.Range(0, 5), _delayBetweenAttack));
+                break;
+
+            case EBossState.ATTACK_ONE:
+                break;
+
+            case EBossState.ATTACK_TWO:
+                break;
+
+            case EBossState.ATTACK_THREE:
+                break;
+
+            case EBossState.ATTACK_FOUR:
+                break;
+
+            case EBossState.VULNERABLE:
+                break;
+
+            case EBossState.DEATH:
+                break;
         }
+
+        _previousState = _currentState;
+    }
+
+    public void OnInitializationEnd()
+    {
+
+    }
+
+    public void OnRecoverEnd()
+    {
+        StartCoroutine(DelayState(EBossState.CHOSE_ATTACK, 0.5f));
+    }
+
+    private IEnumerator DelayState(EBossState nextState, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _currentState = nextState;
     }
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -7,21 +7,45 @@ public class PlayerManager : MonoBehaviour
     #region Variables
     private static PlayerManager Instance;
 
+    public static Color32 ColorBlue => Instance._color1;
+    public static Color32 ColorRed => Instance._color2;
+    public static Color32 ColorGreen => Instance._color3;
+    public static Color32 ColorYellow => Instance._color4;
     private static List<PlayerInfo> PlayerInfos => Instance._setupPlayers;
+
+    [Header("Color Reference")]
+    [SerializeField] private Color32 _color1;
+    [SerializeField] private Color32 _color2;
+    [SerializeField] private Color32 _color3;
+    [SerializeField] private Color32 _color4;
     [SerializeField] private List<PlayerInfo> _setupPlayers;
+
+    public enum EPlayerColor
+    {
+        BLUE,
+        GREEN,
+        RED,
+        YELLOW
+    }
 
     [System.Serializable]
     public class PlayerInfo
     {
         public int ID { get; private set; }
 
-        public Color32 Color1 => _firstColor;
-        public Color32 Color2 => _secondColor;
-        public Color32 Color3 => _thirdColor;
+        [SerializeField] private bool _buttonInverted;
 
-        [SerializeField] private Color32 _firstColor;
-        [SerializeField] private Color32 _secondColor;
-        [SerializeField] private Color32 _thirdColor;
+        [Header("Left Button")]
+        [SerializeField] private EPlayerColor _colorLeft;
+        [SerializeField] private KeyCode _keyLeft;
+
+        [Header("RIght Button")]
+        [SerializeField] private EPlayerColor _colorRight;
+        [SerializeField] private KeyCode _keyRighr;
+
+        [Header("Verticale Button")]
+        [SerializeField] private EPlayerColor _colorVerticale;
+        [SerializeField] private KeyCode _keyVerticale;
 
         public void SetPlayerID(int newId) => ID = newId;
     }
